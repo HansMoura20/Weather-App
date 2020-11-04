d14dc6974a7deb6565d7010f44185457c8c402c8
HansMoura20 <hansadrienmoura@outlook.com>
2020-11-04 13:39:20 +0800

    get weather information of a city from the api

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,5 +1,20 @@
 const key ='SwYQ4WtEP7sTDGwXMppK1ExcyA2YCe0a';
 
+//2.get weather information
+const getWeather = async (id) =>{
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+
+
+
+//1.getcity information
 const getCity = async (city) =>{
   //this is the base url that we want to make a request
   const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
@@ -15,6 +30,6 @@ const getCity = async (city) =>{
   return data[0];
 };
 
-getCity('manchester')
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
+
+  
+

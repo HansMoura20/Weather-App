0553b462f0cf43553bee9c9c64069ab8aca876b8
HansMoura20 <hansadrienmoura@outlook.com>
2020-11-04 15:23:39 +0800

    added the day/night icons and weather icon and then added the country of the searched city

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,19 +1,5 @@
 const key ='SwYQ4WtEP7sTDGwXMppK1ExcyA2YCe0a';
 
-//2.get weather information
-const getWeather = async (id) =>{
-  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-  const query = `${id}?apikey=${key}`;
-
-  const response = await fetch(base + query);
-  const data = await response.json();
-
-  return data[0];
-};
-
-
-
-
 //1.getcity information
 const getCity = async (city) =>{
   //this is the base url that we want to make a request
@@ -30,6 +16,17 @@ const getCity = async (city) =>{
   return data[0];
 };
 
+
+//2.get weather information
+const getWeather = async (id) =>{
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
 
   
 
